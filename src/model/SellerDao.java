b8855598a49a0754b4c5f96bf3891039ac8100b4
KelvinHenri33 <kelvincunha.1024@gmail.com>
2026-06-18 17:07:54 -0300

    DepartmentDao and SellerDao interfaces

---
+++ src/model/SellerDao.java
@@ -0,0 +1,13 @@
+package model;
+
+import entities.Seller;
+
+import java.util.List;
+
+public interface SellerDao {
+    void insert(Seller obj);
+    void update(Seller obj);
+    void deleteById(Seller obj);
+    Seller findById(Integer id);
+    List<Seller> findAll();
+}

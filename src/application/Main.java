89f3020801afaa8feebfd4ad317f176607388c68
KelvinHenri33 <kelvincunha.1024@gmail.com>
2026-06-18 17:48:34 -0300

    findById implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,12 +12,8 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Department obj = new Department(1, "books");
-        System.out.println(obj);
-
-        Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(),3000.0,obj);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
 
--- src/model/dao/impl/DaoFactory.java
+++ src/model/dao/impl/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.dao.impl;
 
+import db.DB;
 import model.SellerDao;
 
 public class DaoFactory {
 
     public  static SellerDao createSellerDao(){
-        return new SellerDaoJDBC();
+        return new SellerDaoJDBC(DB.getConnection());
     }
 
 
